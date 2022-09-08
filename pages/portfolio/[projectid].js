417aae909bf7bd1568bc0f7d64f4eef56f8e5f41
Paul Zijlmans <paulzijlmans@hotmail.com>
2022-09-08 09:10:43 +0200

    Extract Dynamic Path Segment Data

--- pages/portfolio/[projectid].js
+++ pages/portfolio/[projectid].js
@@ -1,4 +1,11 @@
+import { useRouter } from 'next/router';
+
 function PortfolioProjectPage() {
+  const {pathname, query} = useRouter();
+
+  console.log({pathname});
+  console.log({query});
+
   return (
     <div>
       <h1>The Portfolio Project Page</h1>
